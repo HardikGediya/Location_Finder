a4e923af13b8474087e6b80dfb8594766563eb1a
Hardik Gediya <hardikgediya9999@gmail.com>
2022-06-05 16:07:00 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:pr1_locator_app/screens/home_screen.dart';
+import 'package:pr1_locator_app/screens/webApp_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/' : (context) => const HomeScreen(),
+        'map' : (context) => const GoogleMapScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/webApp_screen.dart
@@ -0,0 +1,182 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+import '../models/models.dart';
+
+class GoogleMapScreen extends StatefulWidget {
+  const GoogleMapScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GoogleMapScreen> createState() => _GoogleMapScreenState();
+}
+
+class _GoogleMapScreenState extends State<GoogleMapScreen> {
+
+  final GlobalKey googleMapWebViewKey = GlobalKey();
+  final TextEditingController mapSearchController = TextEditingController();
+
+  double mapProgress = 0;
+
+  InAppWebViewController? mapInAppWebViewController;
+  late PullToRefreshController mapPullToRefreshController;
+
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+      ));
+
+  wikiInitRefreshController() async {
+    mapPullToRefreshController = PullToRefreshController(
+        options: PullToRefreshOptions(color: Colors.blue),
+        onRefresh: () async {
+          if (Platform.isAndroid) {
+            mapInAppWebViewController?.reload();
+          } else if (Platform.isIOS) {
+            mapInAppWebViewController?.loadUrl(
+                urlRequest: URLRequest(
+                    url: await mapInAppWebViewController?.getUrl()));
+          }
+        });
+  }
+
+  @override
+  initState() {
+    super.initState();
+    wikiInitRefreshController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[700],
+        title: const Text('Google Map'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed('/');
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back_ios_rounded),
+            onPressed: () async {
+              await mapInAppWebViewController!.goBack();
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.arrow_forward_ios_rounded),
+            onPressed: () async {
+              await mapInAppWebViewController!.goForward();
+            },
+          ),
+          IconButton(
+            icon: const Icon(CupertinoIcons.refresh,),
+            onPressed: () async {
+              if (Platform.isAndroid) {
+                mapInAppWebViewController?.reload();
+              } else if (Platform.isIOS) {
+                mapInAppWebViewController?.loadUrl(
+                    urlRequest: URLRequest(
+                        url: await mapInAppWebViewController?.getUrl()));
+              }
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.white,
+                child: TextField(
+                  controller: mapSearchController,
+                  onSubmitted: (val) async {
+                    Uri uri = Uri.parse(val);
+                    if (uri.scheme.isEmpty) {
+                      uri = Uri.parse("https://www.google.co.in/search?q=$val");
+                    }
+                    await mapInAppWebViewController!
+                        .loadUrl(urlRequest: URLRequest(url: uri));
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Search on web...",
+                    prefixIcon: const Icon(Icons.search,color: Colors.blue,),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: const BorderSide(color: Colors.blue),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: const BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          (mapProgress < 1)
+              ? LinearProgressIndicator(
+            value: mapProgress,
+            color: Colors.green[700],
+          )
+              : Container(),
+          Expanded(
+            flex: 10,
+            child: InAppWebView(
+              key: googleMapWebViewKey,
+              pullToRefreshController: mapPullToRefreshController,
+              onWebViewCreated: (controller) {
+                mapInAppWebViewController = controller;
+              },
+              initialOptions: options,
+              initialUrlRequest:
+              URLRequest(url: Uri.parse("https://www.google.co.in/search?q=$lat,$long")),
+              onLoadStart: (controller, uri) {
+                setState(() {
+                  mapSearchController.text =
+                  "${uri!.scheme}://${uri.host}${uri.path}";
+                });
+              },
+              onLoadStop: (controller, uri) {
+                mapPullToRefreshController.endRefreshing();
+                setState(() {
+                  mapSearchController.text =
+                  "${uri!.scheme}://${uri.host}${uri.path}";
+                });
+              },
+              androidOnPermissionRequest:
+                  (controller, origin, resources) async {
+                return PermissionRequestResponse(
+                  resources: resources,
+                  action: PermissionRequestResponseAction.GRANT,
+                );
+              },
+              onProgressChanged: (controller, val) {
+                if (val == 100) {
+                  mapPullToRefreshController.endRefreshing();
+                }
+                setState(() {
+                  mapProgress = val / 100;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
